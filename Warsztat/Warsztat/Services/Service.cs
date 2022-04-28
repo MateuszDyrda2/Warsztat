3c09a7fa2b62e9d9ccb6ce0d96df18fabf1098f0
unknown <jakub_nowak2000@o2.pl>
2022-04-29 00:24:53 +0200

    Added worker view frontend/backend

---
+++ Warsztat/Warsztat/Services/Service.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Warsztat.Models;
+
+namespace Warsztat
+{
+    public class Service
+    {
+        private ApplicationContext context;
+        public Service()
+        {
+            context = new ApplicationContext();
+        }
+        public List<Dictionary<string, dynamic>> WorkerActivitities(int workerID)
+        {
+            Dictionary<string, dynamic> dictionary = new Dictionary<string, dynamic>();
+            List<Dictionary<string, dynamic>> list = new List<Dictionary<string, dynamic>>();
+
+            var activities = context.Personels
+                .Where(p => p.personelId == workerID)
+                .First().Activities.ToList();
+
+            foreach(var activity in activities)
+            {
+                dictionary.Add("type", activity.activityType);
+                dictionary.Add("sequenceNumber", activity.sequenceNumber);
+                dictionary.Add("description", activity.description);
+                dictionary.Add("result", activity.result);
+                dictionary.Add("status", activity.status);
+                dictionary.Add("dateTimeOfActivityStart", activity.dateTimeOfActivityStart);
+                dictionary.Add("dateTimeOfActivityEnd", activity.dateTimeOfActivityEnd);
+            }
+            return list;
+        }
+        public string ActivityNameFromDictionary(string activityType)
+        {
+            return context.ActivityDictionaries
+                .Where(activity => activity.activityType == activityType)
+                .First().activityName;
+        }
+        public Dictionary<string, dynamic> PursueWorkerActivity(int indexOfActivity, int workerID)
+        {
+            Activity changedActivity = context.Personels
+                .Where(p => p.personelId == workerID)
+                .First().Activities.ElementAt(indexOfActivity);
+            changedActivity.dateTimeOfActivityEnd = DateTime.Now;
+            changedActivity.status = 1;
+            context.SaveChanges();
+
+            Dictionary<string, dynamic> dictionary = new Dictionary<string, dynamic>();
+            dictionary.Add("type", changedActivity.activityType);
+            dictionary.Add("sequenceNumber", changedActivity.sequenceNumber);
+            dictionary.Add("description", changedActivity.description);
+            dictionary.Add("result", changedActivity.result);
+            dictionary.Add("status", changedActivity.status);
+            dictionary.Add("dateTimeOfActivityStart", changedActivity.dateTimeOfActivityStart);
+            dictionary.Add("dateTimeOfActivityEnd", changedActivity.dateTimeOfActivityEnd);
+            return dictionary;
+        }
+        public Dictionary<string, dynamic> CancelWorkerActivity(int indexOfActivity, int workerID)
+        {
+            Activity changedActivity = context.Personels
+                .Where(p => p.personelId == workerID)
+                .First().Activities.ElementAt(indexOfActivity);
+            changedActivity.dateTimeOfActivityEnd = DateTime.Now;
+            changedActivity.status = 0;
+            context.SaveChanges();
+
+            Dictionary<string, dynamic> dictionary = new Dictionary<string, dynamic>();
+            dictionary.Add("type", changedActivity.activityType);
+            dictionary.Add("sequenceNumber", changedActivity.sequenceNumber);
+            dictionary.Add("description", changedActivity.description);
+            dictionary.Add("result", changedActivity.result);
+            dictionary.Add("status", changedActivity.status);
+            dictionary.Add("dateTimeOfActivityStart", changedActivity.dateTimeOfActivityStart);
+            dictionary.Add("dateTimeOfActivityEnd", changedActivity.dateTimeOfActivityEnd);
+            return dictionary;
+        }
+    }
+}
---
+++ Warsztat/Warsztat/View/Worker.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Warsztat.Models;
+
+namespace Warsztat
+{
+    public partial class Worker : Page
+    {
+        private int _workerId;
+        private Service _service;
+        public Worker(int workerId, Service service)
+        {
+            InitializeComponent();
+            _workerId = workerId;
+            _service = service;
+            List<Dictionary<string, dynamic>> activities = _service.WorkerActivitities(_workerId);
+            for (int i = 0; i < activities.Count; i++)
+            {
+                string name = _service.ActivityNameFromDictionary(activities[i]["activityType"]);
+                int sequenceNumber = activities[i]["sequenceNumber"];
+                string description = activities[i]["description"];
+                string result = activities[i]["result"];
+                int status = activities[i]["status"];
+                DateTime start = activities[i]["dateTimeOfActivityStart"];
+                DateTime end = activities[i]["dateTimeOfActivityEnd"];
+                Activities.Items.Add($"{name}\n" +
+                    $"{description}\n" +
+                    $"Sequence Number: {sequenceNumber}\n" +
+                    $"Status: {status}\n" +
+                    $"Result: {result}\n" +
+                    $"{start} - {end}");
+            }     
+        }
+
+        private void PursueButton_Click(object sender, RoutedEventArgs e)
+        {
+            int numberOfSelectedItems = Activities.SelectedItems.Count;
+            for (int i = 0; i < numberOfSelectedItems; i++)
+            {
+                //index is 0 becouse RemoveAt removes Selected Item, so next item will have index 0
+                int changedItemIndex = Activities.Items.IndexOf(Activities.SelectedItems[0]);
+                Activities.Items.RemoveAt(changedItemIndex);
+                Dictionary<string, dynamic> activity = _service.PursueWorkerActivity(changedItemIndex, _workerId);
+
+                string name = _service.ActivityNameFromDictionary(activity["activityType"]);
+                int sequenceNumber = activity["sequenceNumber"];
+                string description = activity["description"];
+                string result = activity["result"];
+                int status = activity["status"];
+                DateTime start = activity["dateTimeOfActivityStart"];
+                DateTime end = activity["dateTimeOfActivityEnd"];
+                Activities.Items.Add($"{name}\n" +
+                    $"{description}\n" +
+                    $"Sequence Number: {sequenceNumber}\n" +
+                    $"Status: {status}\n" +
+                    $"Result: {result}\n" +
+                    $"{start} - {end}");
+            }
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            int numberOfSelectedItems = Activities.SelectedItems.Count;
+            for (int i = 0; i < numberOfSelectedItems; i++)
+            {
+                //index is 0 becouse RemoveAt removes Selected Item, so next item will have index 0
+                int changedItemIndex = Activities.Items.IndexOf(Activities.SelectedItems[0]);
+                Activities.Items.RemoveAt(changedItemIndex);
+                Dictionary<string, dynamic> activity = _service.CancelWorkerActivity(changedItemIndex, _workerId);
+
+                string name = _service.ActivityNameFromDictionary(activity["activityType"]);
+                int sequenceNumber = activity["sequenceNumber"];
+                string description = activity["description"];
+                string result = activity["result"];
+                int status = activity["status"];
+                DateTime start = activity["dateTimeOfActivityStart"];
+                DateTime end = activity["dateTimeOfActivityEnd"];
+                Activities.Items.Add($"{name}\n" +
+                    $"{description}\n" +
+                    $"Sequence Number: {sequenceNumber}\n" +
+                    $"Status: {status}\n" +
+                    $"Result: {result}\n" +
+                    $"{start} - {end}");
+            }
+        }
+    }
+}
