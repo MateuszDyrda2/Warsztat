f88b94bf80f13c10b375c2b1d780e46bdabf4434
Kamil Niedziela <kaminie318@student.polsl.pl>
2022-05-09 00:15:40 +0200

    Some features

--- Warsztat/Warsztat/Views/LoginView.xaml.cs
+++ Warsztat/Warsztat/Views/LoginView.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Warsztat
 {
@@ -25,12 +12,20 @@ namespace Warsztat
             InitializeComponent();
         }
 
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(usernameTextbox.Text.Length == 0)
+            {
+                MessageBox.Show("Field with username is empty!");
+            }
+            if(passwordTextbox.Password.Length == 0)
+            {
+                MessageBox.Show("Field with password is empty!");
+            }
+
+        }
+
         //private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        //{
-
-        //}
-
-        //private void Button_Click(object sender, RoutedEventArgs e)
         //{
 
         //}
