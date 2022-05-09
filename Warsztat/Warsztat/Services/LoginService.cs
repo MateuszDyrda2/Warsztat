230d93f439ccf2309d3101bc4e7972b66edd2def
Kamil Niedziela <kaminie318@student.polsl.pl>
2022-05-09 20:04:28 +0200

    Error while parsing personel list

---
+++ Warsztat/Warsztat/Services/LoginService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Warsztat.Models;
+
+namespace Warsztat.Services
+{
+    public partial class Service
+    {
+        public List<Personel> checkAllPersonel()
+        {
+            List<Personel> personels = new();
+
+            var personelsDB = context.Personels.ToList();
+
+            foreach (var personelDB in personelsDB)
+            {  
+               personels.Add(new Personel()
+               {
+                   Id = personelDB.personelId,
+                   Name = personelDB.name,
+                   Surname = personelDB.surrname,
+                   PhoneNumber = personelDB.phoneNumber,
+                   Role = personelDB.role,
+                   Username = personelDB.username,
+                   Password = personelDB.password
+               });
+            }
+
+            return personels;
+        }
+    }
+}
---
+++ Warsztat/Warsztat/View/LoginView.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using Warsztat.Services;
+using Warsztat.View;
+
+namespace Warsztat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class LoginView : Window
+    {
+        public Service Service { get; set; }
+        public DataTransfer transferDelegate;
+        private MyPopup? currentPopup;
+        public LoginView()
+        {
+            InitializeComponent();
+
+            //for testing
+            Service = new();
+
+            if (Service == null)
+                throw new NullReferenceException();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(usernameTextbox.Text.Length == 0 || passwordTextbox.Password.Length == 0)
+            {
+                MessageBox.Show("Field with username or password is empty!");
+            }
+            else
+            {
+
+                List<Service.Personel> personels = Service.checkAllPersonel();
+
+                foreach (Service.Personel personel in personels)
+                {
+                    if(personel.Username == usernameTextbox.Text && personel.Password == passwordTextbox.Password)
+                    {
+                        MessageBox.Show("Logged as " + personel.Username + " !");
+                        if(personel.Role=="Admin")
+                        {
+                            Admin adminView = new Admin();
+                            this.Close();
+                            adminView.Show();
+                        }
+                        else if (personel.Role == "Manager")
+                        {
+                            Manager managerView = new Manager();
+                            this.Close();
+                            managerView.Show();
+                        }
+                        else
+                        {
+                            Worker workerView = new Worker();
+                            this.Close();
+                            //workerView.Show();        //O co mu tu chodzi??? XD
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("There isn't such user! Try again");
+                    }
+                }
+            }
+
+        }
+
+        //private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        //{
+
+        //}
+    }
+}
